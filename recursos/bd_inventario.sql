8f2da55733de23f6380a80cc3739206f4ede4d85
Gerson <82684569+Gerson-Law@users.noreply.github.com>
2021-07-01 13:50:37 -0600

    Add files via upload

---
+++ recursos/bd_inventario.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-07-2021 a las 08:35:44
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_inventario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_categoria`
+--
+
+CREATE TABLE `tb_categoria` (
+  `id_categoria` int(5) NOT NULL,
+  `nom_categoria` varchar(50) NOT NULL,
+  `estado_categoria` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tb_categoria`
+--
+
+INSERT INTO `tb_categoria` (`id_categoria`, `nom_categoria`, `estado_categoria`) VALUES
+(1, 'Vegetales', 1),
+(3, 'Mariscos', 1),
+(4, 'Lacteos', 1),
+(5, 'Bebidas Naturales', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_producto`
+--
+
+CREATE TABLE `tb_producto` (
+  `id_producto` int(9) NOT NULL,
+  `nom_producto` varchar(55) NOT NULL,
+  `stock` decimal(3,2) DEFAULT NULL,
+  `precio` decimal(3,2) DEFAULT NULL,
+  `unidad_de_medida` varchar(20) DEFAULT NULL,
+  `estado_producto` tinyint(1) DEFAULT NULL,
+  `categoria` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tb_categoria`
+--
+ALTER TABLE `tb_categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `tb_producto`
+--
+ALTER TABLE `tb_producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tb_categoria`
+--
+ALTER TABLE `tb_categoria`
+  MODIFY `id_categoria` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_producto`
+--
+ALTER TABLE `tb_producto`
+  MODIFY `id_producto` int(9) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tb_producto`
+--
+ALTER TABLE `tb_producto`
+  ADD CONSTRAINT `tb_producto_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `tb_categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
