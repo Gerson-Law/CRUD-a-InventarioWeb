fc581e8a069ed43ec93819b3a6f9651aacf7b425
Stefanyceron <86733009+Stefanyceron@users.noreply.github.com>
2021-07-01 17:34:44 -0600

    Update Categorias.java

--- src/java/Controller/Categorias.java
+++ src/java/Controller/Categorias.java
@@ -1,4 +1,4 @@
-/*
+U/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -80,5 +80,7 @@ public class Categorias extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+//maneja los datos
 
 }
